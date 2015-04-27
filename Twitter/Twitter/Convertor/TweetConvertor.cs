df5b3b6cfa2ab956bdad12feab06f3832c272597
epopodneac <evghenip@gmail.com>
2015-04-27 15:27:33 +0300

    Adding TweetConverter

---
+++ Twitter/Twitter/Convertor/TweetConvertor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Twitter.DAL;
+using Twitter.Models;
+
+namespace Twitter.Convertor
+{
+    public class TweetConvertor
+    {
+        public Tweet ConvertTweetToDAL(TweetModel convertedTweet)
+        {
+            Tweet currentTweet = new Tweet();
+            currentTweet.descripton = convertedTweet.Descripton;
+            currentTweet.id_user = convertedTweet.IdUser;
+            return currentTweet;
+        }
+
+        public TweetModel ConvertTweetToModel(Tweet convertedTweet)
+        {
+            TweetModel currentTweet = new TweetModel();
+            currentTweet.Descripton = convertedTweet.descripton;
+            currentTweet.IdUser = convertedTweet.id_user;
+            currentTweet.CreatedOn = convertedTweet.created_on;
+            return currentTweet;
+        }
+
+    }
+}
--- Twitter/Twitter/Models/TweetModel.cs
+++ Twitter/Twitter/Models/TweetModel.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Twitter.Models
 {
     public class TweetModel
     {
+        [Required(ErrorMessage = "Please enter the message", AllowEmptyStrings = false)]
+        [StringLength(140, ErrorMessage = "The message value cannot exceed 140 characters")]
         public string Descripton { get; set; }
         public int IdUser { get; set; }
         public byte[] CreatedOn { get; set; }
